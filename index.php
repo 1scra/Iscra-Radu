feb0f63478dbe48790a10aea59cb8514fe03a486
Radu ISCRA <iscra.r000@gmail.com>
2023-12-16 15:09:57 +0100

    Actualisation du version v1

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Iscra Radu</title>
+  <?php include("Back-end\links-connect.php");  ?>
+</head>
+<body>
+
+  <div class="d-flex container-fluid w-100">
+   
+      <!------------NAV------------->
+      <div class="nav d-flex flex-column justify-content-center container">
+       
+          <a href="index.php"><span class="material-symbols-outlined">window</span>Menu</a>
+          <a href="suivie_facture.php"><span class="material-symbols-outlined">description</span>Mes Notes de Frais</a>
+          <a href="saisie_frais.php"><span class="material-symbols-outlined">note_add</span>Nouvelles Saisies</a>
+        
+      </div>
+      <!------------Autre contenue----->
+      <div class="container">
+        d
+      </div>
+   
+
+
+  </div>
+
+
+
+
+
+  <?php include("Back-end\scripts.php");  ?>
+</body>
+</html>
